439908fca1da7b0143ca625472c4150564de6b8c
AH-dark <ahdark@outlook.com>
2022-05-10 18:06:27 +0800

    Feat: logger and cors settings

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
 import express from "express";
+import cors from "cors";
+import logger from "morgan";
 import { port } from "./global";
 import Screenshot from "./application/Screenshot";
 
 const app = express();
+
+app.use(
+    cors((req, callback) => {
+        callback(null, {
+            credentials: true,
+            origin: true,
+            maxAge: 3600,
+            allowedHeaders: ["Authorized", "Content-Type"],
+            exposedHeaders: "*",
+        });
+    })
+);
+app.use(logger("combined"));
+app.use((req, res, next) => {
+    res.setHeader("X-Powered-By", "github.com/ah-dark/website-screenshot");
+    next();
+});
 
 app.use(Screenshot);
 
